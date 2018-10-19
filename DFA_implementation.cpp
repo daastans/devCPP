f4356b3ef4f885939c11dabc5f25c6576f7e7bef
Aman Sharma <AmanSharma.pcmip@outlook.com>
2018-10-19 11:22:55 +0530

    Removing test cases & adding Intro

--- DFA_implementation.cpp
+++ DFA_implementation.cpp
@@ -1,3 +1,6 @@
+/*This DFA checks if whether input string string starts or ends with 'a' or NOT
+No of characters as well are characters are generated randomly using rand() functio*/
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(void)
@@ -11,23 +14,23 @@ int main(void)
   {
     char c= 'a'+rand()%2;      //generating random character a or b
     i++;                       //chracter counter
-    cout<<i<<" character is read "<<c<<"\n";
+    cout<<c<<" ";
 
 
     if(c=='a')              //checking if characters starts with a
     {
       if (i==max)         //if only one character is present state
-        cout<<"\n String accepted \nonlY  A present in string\n";
+        cout<<"\n String accepted \nonlY  a present in string\n";
       while(i<max)        //loop reading for more than one characters
       {
         c='a'+rand()%2;
         i++;
-        cout<<i<<" character is read "<<c<<"\n";
+        cout<<c<<" ";
 
         if(i==max&&c=='a')      //checking if last character is a
-          cout<<"\nString Accepted\nFirst and Last character is A\n";
+          cout<<"\nString Accepted\nFirst and Last character is a\n";
         else if(i==max)         //checking if last character is not a
-          cout<<"\nString Rejected \nFirst character is A but last is B\n";
+          cout<<"\nString Rejected \nFirst character is a but last is b\n";
       }
     }
     else      //if start character is not a
@@ -38,7 +41,7 @@ int main(void)
          i++;
          cout<<c<<" ";
       }
-      cout<<"\nString REjected \n Does NOt start with A"<<endl;
+      cout<<"\nString REjected \n Does NOt start with a"<<endl;
     }
   }
   return 0;
