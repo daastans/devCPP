8d5bef5001efa881bfb0c3910af76504b1fb06d1
Aman Sharma <amansharma.pcmip@gmail.com>
2019-04-12 22:39:27 +0530

    First bulletproffing effort

--- input_bullet_proofing.cpp
+++ input_bullet_proofing.cpp
@@ -4,7 +4,11 @@ using namespace std;
 int main() {
   int a, b;
   cout << "enter a: ";
-  cin >> a;
+  while(!(cin >> a))
+  {
+    cout<<"Invalid input\n";
+    cin.clear();
+  }
   cout << "enter b: ";
   cin >> b;
   cout << "a * b = " << a * b << endl;
